bf9f651626e8694c2558647eba151d00fb97a564
mohammadghulamqadir <mohammadghulam.qadir@gmail.com>
2024-07-07 19:47:52 +0500

    Add server

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose"
-import express from "express"
+import express, { Router } from "express"
 import dotenv from "dotenv";
 import monodbConnect from "./app/utils/db.js"
 
 dotenv.config();
 const app = express()
 const PORT = process.env.PORT;
+const router = Router()
+
 
 app.listen( PORT, () => {
     console.log( "server is running on PORT 5000" );
